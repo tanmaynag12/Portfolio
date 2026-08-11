6ab21b8b03e88bc29eb2c25fb8c25193bcfed5ab
tanmaynag12 <nagtanmay01@gmail.com>
2026-08-11 15:11:58 +0530

    added hero section

---
+++ src/Hero.tsx
@@ -0,0 +1,14 @@
+function Hero() {
+  return (
+    <div>
+      <h1>Tanmay Nag</h1>
+      <span>Software Engineer</span>
+      <p>
+        Hello! I'm Tanmay, an Artificial Intelligence and Data Science Graduate.
+        I enjoy turning ideas into full working applications.
+      </p>
+    </div>
+  );
+}
+
+export default Hero;
